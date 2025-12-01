022101a6677ced4cfcabb08b8ced8b48170c6234
15616 <1561621097@qqcom>
2025-12-01 17:03:37 +0800

    fix(ci): 移除 dependencyResolutionManagement 配置

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,11 +19,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 }
 
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
-}
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
